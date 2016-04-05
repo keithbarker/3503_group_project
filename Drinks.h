b51e4e9c40e6bc225f80315c2e53c95ab47a5a2a
Peter2036 <peterlrose3@gmail.com>
2016-04-05 16:43:52 -0400

    drinks default constructor added back

--- Drinks.h
+++ Drinks.h
@@ -5,7 +5,8 @@ class Drinks: public Item
 private:
     bool IceIce = false;    //default drink has no ice
 public:
-	Drinks();
+    Drinks();
+    Drinks(int itemsLeft, double price, string name, string description, string size, string flavor, int quantity);
     bool hasIce();
     void setIce();
 };
